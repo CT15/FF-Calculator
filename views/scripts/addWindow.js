0623c0047858154ad677b29b56dc4b08147ec2ee
Calvin Tantio <calvin.tantio@outlook.com>
2018-08-12 00:56:37 +0800

    Storage new student donwne

--- views/scripts/addWindow.js
+++ views/scripts/addWindow.js
@@ -1,12 +1,61 @@
 const electron = require('electron');
 const { ipcRenderer } = electron;
+const { dialog } = electron.remote;
+const Datastore = require('nedb');
+const studentsDb = new Datastore({ filename: 'storage/students.db', autoload: true });
 
 const form = document.querySelector('form');
 form.addEventListener('submit', submitForm);
 
 function submitForm(e) {
     e.preventDefault();
-    const name = document.querySelector('#name').value;
+
+    let name = document.querySelector('#name').value;
     const deposit = document.querySelector('#deposit').value;
+
+    if (!name || !deposit) {
+        dialog.showMessageBox({
+            type: "error",
+            title: "Missing field(s)",
+            message: "All fields must be filled."
+        });
+        return;
+    }
+
+    name = toTitleCase(name);
+
+    isPresent(name, (present) => {
+        if (present) {
+            dialog.showMessageBox({
+                type: "error",
+                title: "Duplicate student name",
+                message: "Student with name " + name + " is already present."
+            });
+            return;
+        }
+    });
+
+    saveStudent(name, deposit);
+
     ipcRenderer.send('deposit:add', name, deposit);
+}
+
+function isPresent(name, callback) {
+    studentsDb.findOne({ name: name }, (err, doc) => {
+        if (doc) { callback(true); } else { callback(false); }
+    });
+}
+
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, (txt) => {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+function saveStudent(name, deposit) {
+    const newStudent = {
+        name: name,
+        deposit: deposit
+    }
+    studentsDb.insert(newStudent);
 }
